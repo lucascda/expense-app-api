96000a542f40fbf360e5fa1f0d5c5ab9f2fd5978
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-12-09 21:33:27 -0300

    generate user schema

---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import {
+  pgTable,
+  primaryKey,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 320 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
