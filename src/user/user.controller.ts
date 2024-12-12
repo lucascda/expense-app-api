ef5259c523dd7d811d20c7d2aa0bbc2ae6778e4d
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-12-12 18:40:43 -0300

    refactor: call next middleware after catching error

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,37 +1,31 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { createUserService } from "./user.service";
 
 export const createUserController = (
   userService: ReturnType<typeof createUserService>
 ) => ({
-  async createUser(req: Request, res: Response) {
+  async createUser(req: Request, res: Response, next: NextFunction) {
     try {
       const user = await userService.create(req.body);
       res.status(201).json(user);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(400).json({ error: error.message, name: error.name });
-      }
+      next(error);
     }
   },
-  async signIn(req: Request, res: Response) {
+  async signIn(req: Request, res: Response, next: NextFunction) {
     try {
       const token = await userService.signIn(req.body);
       res.status(200).json(token);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(400).json({ error: error.message, name: error.name });
-      }
+      next(error);
     }
   },
-  async profile(req: Request, res: Response) {
+  async profile(req: Request, res: Response, next: NextFunction) {
     try {
       const user = await userService.findById((req as any).user_id);
       res.status(200).json(user);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(400).json({ error: error.message, name: error.name });
-      }
+      next(error);
     }
   },
 });
